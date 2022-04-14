943619d7207ee57f8736d29dbb20adfd7517d995
Roberto C Melchor <rmelchor151@gmail.com>
2022-04-14 13:08:11 -0400

    skeleton card WIP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import TopNav from "./components/TopNav";
+import { EventCard } from './components/Event Card';
 // import Dogs from "./pages/Dogs";
 // import Cats from "./pages/Cats";
 // import Sheeps from "./pages/Sheeps";
@@ -11,8 +12,9 @@ function App() {
     <div className="App">
       <Router>
         <TopNav />
+        <EventCard />
         {/* <Switch> */}
-          {/* <Route path="/" exact component={Home} />
+        {/* <Route path="/" exact component={Home} />
           <Route path="/" component={Cats} />
           <Route path="/" component={Sheeps} />
           <Route path="/goats" component={Goats} /> */}
--- src/components/Event Card/EventCard.jsx
+++ src/components/Event Card/EventCard.jsx
@@ -1,16 +1,22 @@
 //Reusable component for displaying events on the dashboard page.
 //Include details about the event and each card should take its respective detailed event page
 
+import { BsThreeDotsVertical } from 'react-icons/bs'
+import Celebration from '../../image/celebration.svg'
+
 const EventCard = () => {
-    return <div>
-        <div>
-            <h3>Lorem Ipsum filler text</h3>
-            <href class='rotate-180'>...</href>
+    return <div className='w-full h-40 border-2 border-black rounded-xl px-2'>
+        <div className='flex relative justify-center'>
+            <h3>filler text</h3>
+            <BsThreeDotsVertical className='absolute right-2 top-2 ' />
         </div>
-        <img>
-        </img>
-        <div>
-            <p></p>
+        <div className='flex'>
+            <div className='w-32 h-32'>
+                <img className='w-full h-full' src={Celebration} alt='celebration image' />
+            </div>
+            <div className='w-full h-5'>
+                <p className=' w-full h-full break-words leading-tight text-ellipsis'>Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            </div>
         </div>
     </div>
 }
