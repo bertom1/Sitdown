7ec5c9746456d0861dcbe23d5fd08ea7ed1e49e4
Roberto C Melchor <rmelchor151@gmail.com>
2022-04-22 22:55:50 -0400

    render invite pages

--- src/components/Invite Card/InviteCard.jsx
+++ src/components/Invite Card/InviteCard.jsx
@@ -1,5 +1,6 @@
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { useEvent, useInvite } from '../../Context/EventContext'
+import { useNavigate } from 'react-router-dom'
 import Celebration from '../../image/celebration.svg'
 import '../Event Card/EventCard.css'
 
@@ -7,37 +8,43 @@ import '../Event Card/EventCard.css'
 const InviteCard = ({invite, id}) => {
     const {delInv} = useInvite()
     const {addEvent} = useEvent()
-    const handleAdd = () => {
+    const nav = useNavigate()
+    const handleAdd = (e) => {
+        e.cancelBubble = true
+        e.stopPropagation()
         addEvent(invite)
         delInv(id)
     }
-    const handleDelete = () => {
+    const handleDelete = (e) => {
+        e.cancelBubble = true
+        e.stopPropagation()
+        if (window.confirm('Are you sure you want to decline the invite? \nYou will need to request a new invite if you decide change your mind after deleting.'))
         delInv(id)
     }
-    return <div className='w-full h-48 border-2 border-black rounded-xl px-2 mb-2'>
-        <div className='flex relative justify-center'>
-            <h3 className=''>Invited to: {invite.title}</h3>
-            <BsThreeDotsVertical size={22} className='ml-2 absolute right-2 top-0.5 ' />
-        </div>
-        <div className='flex '>
-            <div className='w-30'>
-                <img className='w-full h-full' src={Celebration} alt='celebration' />
+    return <div className='w-full h-48 border-2 border-black rounded-xl px-2 mb-2' onClick={()=>nav(`/invite/${id}`, {replace: true})}>
+            <div className='flex relative justify-center'>
+                <h3 className=''>Invited to: {invite.title}</h3>
+                <BsThreeDotsVertical size={22} className='ml-2 absolute right-2 top-0.5 ' />
             </div>
-            <div className='w-full mx-2 '>
-                <div className='w-full h-16 '>
-                    <p className='text-left leading-tight line-clamp'>Description: {invite.description}
-                    </p>
+            <div className='flex '>
+                <div className='w-30'>
+                    <img className='w-full h-full' src={Celebration} alt='celebration' />
                 </div>
-                <div className='text-left '>
-                        <p>Date: {invite.date}</p>
-                        <p>Time: {invite.time}</p>
+                <div className='w-full mx-2 '>
+                    <div className='w-full h-16 '>
+                        <p className='text-left leading-tight line-clamp'>Description: {invite.description}
+                        </p>
+                    </div>
+                    <div className='text-left '>
+                            <p>Date: {invite.date}</p>
+                            <p>Time: {invite.time}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className='flex justify-center'>
-                <button type='button' onClick={handleAdd}className='bg-green rounded-lg text-white px-5 py-1 mr-8'>Accept</ button>
-                <button type='button' onClick={handleDelete}className='rounded-lg px-5 py-1 bg-red-500 text-white'>Decline</ button>
-        </div> 
+            <div className='flex justify-center'>
+                    <button type='button' onClick={handleAdd}className='bg-green rounded-lg text-white px-5 py-1 mr-8'>Accept</ button>
+                    <button type='button' onClick={handleDelete}className='rounded-lg px-5 py-1 bg-red-500 text-white'>Decline</ button>
+            </div> 
     </div>
 }
 
--- src/components/Invite Page/InvitePage.jsx
+++ src/components/Invite Page/InvitePage.jsx
@@ -1,17 +1,42 @@
 import Celebration from '../../image/celebration.svg'
 import { useEvent, useInvite } from '../../Context/EventContext'
+import { AiOutlineCalendar, AiOutlineClockCircle } from 'react-icons/ai'
 import { GoogleMap, Marker } from '@react-google-maps/api'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
-const InvitePage = ({invite}) => {
-    const {delInv} = useInvite()
-    const {addEvent} = useEvent()
+const InvitePage = () => {
+    const { invites, delInv } = useInvite()
+    const { addEvent } = useEvent()
+    const nav = useNavigate();
+    let { id } = useParams()
+    id = Number(id)
+    const loader = {
+        title: `Loading event`,
+        date: 'mm/dd/yyyy',
+        time: 'hh:mm',
+        guests: [],
+        myItems: [],
+        items: [],
+        address:'loading address',
+        geolocation:{lat: 0, lng: 0},
+        description: `...`
+    }
+    const [invite, setInvite] = useState(loader)
     const handleAdd = () => {
         addEvent(invite)
-        delInv(invite.id)
+        delInv(id)
+        nav('/', {replace: true})
     }
     const handleDelete = () => {
-        delInv(invite.id)
+        if (window.confirm('Are you sure you want to decline the invite? \nYou will need to request a new invite if you decide change your mind after deleting.')){
+            delInv(id)
+            nav('/', {replace: true})
+        }
     }
+    useEffect(() => {
+        setInvite(invites[id])
+    }, [invites, id])
     const Map = ({loc}) => 
     {
         return <GoogleMap 
@@ -25,14 +50,32 @@ const InvitePage = ({invite}) => {
                 <Marker position={loc} />
             </GoogleMap>
     }
+    const formatAddr = (addr) => {
+        const firstComma = addr.indexOf(',')
+        const street = addr.substring(0, firstComma)
+        const rest = addr.substring(firstComma + 1, addr.length)
+        return <p>
+            {street} <br />
+            {rest}
+        </p>
+    }
     return <div>
         <img className='mx-auto' src={Celebration} alt='celebration'/>
         <div className='text-center '>
             <p>{invite.title}</p>
-            <p>{invite.date}</p>
-            <p>{invite.time}</p>
-            <div className='flex flex-col align-center overflow-visible'>
-            <p>{invite.address}</p>
+            <div className='flex justify-center'>
+                <AiOutlineCalendar size={18} className='mt-1 mr-2' />
+                <p>{invite.date}</p>
+            </div>
+            <div className='flex justify-center' >
+                <AiOutlineClockCircle size={18} className='mt-1 mr-2' />
+                <p>{invite.time}</p>
+            </div>
+            <div className='flex flex-col '>
+            <div className='flex justify-center' >
+                <AiOutlineClockCircle size={18} className='mt-1 mr-1' />
+                {formatAddr(invite.address)}
+            </div>
             <div className='m-auto'>
                 <Map  loc={invite.geolocation} />
             </div>
