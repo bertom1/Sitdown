9f2b0f2aec21d200b079f830a97eefc482b41e07
CodeChelseaCode <officialchelsearice@gmail.com>
2022-04-28 12:08:16 -0400

    update 4/28
    - added visual of chat
    -

---
+++ src/components/Chatbox/index.js
@@ -0,0 +1,10 @@
+var perf =require('./Chatbox.html');
+
+class Chat extends React.Component {
+   render(){
+      return (
+         <iframe src={perf }></iframe>   /* like this */
+      );
+   }
+}
+export default Chat;
